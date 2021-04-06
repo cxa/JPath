5612d9901ef4ba9362afbaecd6d7545622338c12
CHEN Xian-an <xianan.chen@gmail.com>
2021-04-06 14:10:34 +0800

    Formatted with fantomas

--- src/JPath.fs
+++ src/JPath.fs
@@ -3,61 +3,63 @@ namespace JPath
 module JPath =
   open System.Text.Json
 
-  let private valueFor' (key:string) (jsonEl:JsonElement) =
+  let private valueFor' (key: string) (jsonEl: JsonElement) =
     match jsonEl.ValueKind with
-    | JsonValueKind.Object -> jsonEl.GetProperty (propertyName = key)
-    | JsonValueKind.Array -> jsonEl.Item (int (key))
-    | t ->
-      let exn = System.InvalidOperationException ("This value's ValueKind is neither Object nor Array.")
-      raise exn
+    | JsonValueKind.Object -> jsonEl.GetProperty(propertyName = key)
+    | JsonValueKind.Array -> jsonEl.Item(int (key))
+    | _ ->
+      raise (
+        System.InvalidOperationException(
+          $"The `ValueKind` of {jsonEl} is neither Object nor Array."
+        )
+      )
 
   let rec private loop keys jel =
     match keys with
     | [] -> jel
-    | k::rest ->
-      loop rest (valueFor' k jel)
+    | k :: rest -> loop rest (valueFor' k jel)
 
-  let property (keyPath:string) jsonEl =
+  let property (keyPath: string) jsonEl =
     let keys = keyPath.Split '.' |> Array.toList
     loop keys jsonEl
 
-  let private __ (converter: JsonElement -> 'a) (keyPath:string) (jsonEl:JsonElement)  =
+  let private __ (converter: JsonElement -> 'a) (keyPath: string) (jsonEl: JsonElement) =
     property keyPath jsonEl |> converter
 
-  let bool = __ (fun jel -> jel.GetBoolean ())
+  let bool = __ (fun jel -> jel.GetBoolean())
 
-  let byte = __ (fun jel -> jel.GetByte ())
+  let byte = __ (fun jel -> jel.GetByte())
 
-  let bytesFromBase64 = __ (fun jel -> jel.GetBytesFromBase64 ())
+  let bytesFromBase64 = __ (fun jel -> jel.GetBytesFromBase64())
 
-  let dateTime = __ (fun jel -> jel.GetDateTime ())
+  let dateTime = __ (fun jel -> jel.GetDateTime())
 
-  let dateTimeOffset = __ (fun jel -> jel.GetDateTimeOffset ())
+  let dateTimeOffset = __ (fun jel -> jel.GetDateTimeOffset())
 
-  let decimal = __ (fun jel -> jel.GetDecimal ())
+  let decimal = __ (fun jel -> jel.GetDecimal())
 
-  let double = __ (fun jel -> jel.GetDouble ())
+  let double = __ (fun jel -> jel.GetDouble())
 
-  let guid = __ (fun jel -> jel.GetGuid ())
+  let guid = __ (fun jel -> jel.GetGuid())
 
-  let int16 = __ (fun jel -> jel.GetInt16 ())
+  let int16 = __ (fun jel -> jel.GetInt16())
 
-  let int32 = __ (fun jel -> jel.GetInt32 ())
+  let int32 = __ (fun jel -> jel.GetInt32())
 
-  let int64 = __ (fun jel -> jel.GetInt64 ())
+  let int64 = __ (fun jel -> jel.GetInt64())
 
-  let rawText = __ (fun jel -> jel.GetRawText ())
+  let rawText = __ (fun jel -> jel.GetRawText())
 
-  let sbyte = __ (fun jel -> jel.GetSByte ())
+  let sbyte = __ (fun jel -> jel.GetSByte())
 
-  let single = __ (fun jel -> jel.GetSingle ())
+  let single = __ (fun jel -> jel.GetSingle())
 
   let float32 = single
 
-  let string = __ (fun jel -> jel.GetString ())
+  let string = __ (fun jel -> jel.GetString())
 
-  let uint16 = __ (fun jel -> jel.GetUInt16 ())
+  let uint16 = __ (fun jel -> jel.GetUInt16())
 
-  let uint32 = __ (fun jel -> jel.GetUInt32 ())
+  let uint32 = __ (fun jel -> jel.GetUInt32())
 
-  let uint64 = __ (fun jel -> jel.GetUInt64 ())
+  let uint64 = __ (fun jel -> jel.GetUInt64())
--- tests/JPath.fs
+++ tests/JPath.fs
@@ -9,10 +9,8 @@ open JPath
 [<Fact>]
 let ``Test bool for keypath`` () =
   let t = true
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %b}}""" t)
-  jdoc.RootElement
-  |> JPath.bool "a.b"
-  |> should equal t
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %b}}""" t)
+  jdoc.RootElement |> JPath.bool "a.b" |> should equal t
 
 [<Fact>]
 let ``Test bool for keypath with exn`` () =
@@ -23,127 +21,95 @@ let ``Test bool for keypath with exn`` () =
 [<Fact>]
 let ``Test bool for keypath with root array`` () =
   let t = true
-  let jdoc = JsonDocument.Parse (sprintf """[%b]""" t)
-  jdoc.RootElement
-  |> JPath.bool "0"
-  |> should equal t
+  let jdoc = JsonDocument.Parse(sprintf """[%b]""" t)
+  jdoc.RootElement |> JPath.bool "0" |> should equal t
 
 [<Fact>]
 let ``Test byte for keypath`` () =
   let c = byte 'a'
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %i}}""" c)
-  jdoc.RootElement
-  |> JPath.byte "a.b"
-  |> should equal c
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %i}}""" c)
+  jdoc.RootElement |> JPath.byte "a.b" |> should equal c
 
 [<Fact>]
 let ``Test sbyte for keypath`` () =
   let sb = SByte.MaxValue
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": [%i]}}""" sb)
-  jdoc.RootElement
-  |> JPath.sbyte "a.b.0"
-  |> should equal sb
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": [%i]}}""" sb)
+  jdoc.RootElement |> JPath.sbyte "a.b.0" |> should equal sb
 
 [<Fact>]
 let ``Test int16 for keypath`` () =
   let i = Int16.MaxValue
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %i}}""" i)
-  jdoc.RootElement
-  |> JPath.int16 "a.b"
-  |> should equal i
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %i}}""" i)
+  jdoc.RootElement |> JPath.int16 "a.b" |> should equal i
 
 [<Fact>]
 let ``Test uint16 for keypath`` () =
   let ui = UInt16.MaxValue
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %i}}""" ui)
-  jdoc.RootElement
-  |> JPath.uint16 "a.b"
-  |> should equal ui
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %i}}""" ui)
+  jdoc.RootElement |> JPath.uint16 "a.b" |> should equal ui
 
 [<Fact>]
 let ``Test int32 for keypath`` () =
   let i = Int32.MaxValue
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %i}}""" i)
-  jdoc.RootElement
-  |> JPath.int32 "a.b"
-  |> should equal i
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %i}}""" i)
+  jdoc.RootElement |> JPath.int32 "a.b" |> should equal i
 
 [<Fact>]
 let ``Test uint32 for keypath`` () =
   let ui = UInt32.MaxValue
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %i}}""" ui)
-  jdoc.RootElement
-  |> JPath.uint32 "a.b"
-  |> should equal ui
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %i}}""" ui)
+  jdoc.RootElement |> JPath.uint32 "a.b" |> should equal ui
 
 [<Fact>]
 let ``Test int64 for keypath`` () =
   let i = Int64.MaxValue
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %i}}""" i)
-  jdoc.RootElement
-  |> JPath.int64 "a.b"
-  |> should equal i
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %i}}""" i)
+  jdoc.RootElement |> JPath.int64 "a.b" |> should equal i
 
 [<Fact>]
 let ``Test uint64 for keypath`` () =
   let ui = UInt64.MaxValue
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %i}}""" ui)
-  jdoc.RootElement
-  |> JPath.uint64 "a.b"
-  |> should equal ui
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %i}}""" ui)
+  jdoc.RootElement |> JPath.uint64 "a.b" |> should equal ui
 
 [<Fact>]
 let ``Test double for keypath`` () =
   let d = 3.14
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %f}}""" d)
-  jdoc.RootElement
-  |> JPath.double "a.b"
-  |> should equal d
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %f}}""" d)
+  jdoc.RootElement |> JPath.double "a.b" |> should equal d
 
 [<Fact>]
 let ``Test float for keypath`` () =
   let f = 3.14f
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %f}}""" f)
-  jdoc.RootElement
-  |> JPath.float32 "a.b"
-  |> should equal f
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %f}}""" f)
+  jdoc.RootElement |> JPath.float32 "a.b" |> should equal f
 
 [<Fact>]
 let ``Test decimal for keypath`` () =
   let d = 0.7833m
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": %M}}""" d)
-  jdoc.RootElement
-  |> JPath.decimal "a.b"
-  |> should equal d
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": %M}}""" d)
+  jdoc.RootElement |> JPath.decimal "a.b" |> should equal d
 
 [<Fact>]
 let ``Test string for keypath`` () =
   let s = "Test string for keypath"
-  let jdoc = JsonDocument.Parse (sprintf """{ "a": {"b": "%s"}}""" s)
-  jdoc.RootElement
-  |> JPath.string "a.b"
-  |> should equal s
+  let jdoc = JsonDocument.Parse(sprintf """{ "a": {"b": "%s"}}""" s)
+  jdoc.RootElement |> JPath.string "a.b" |> should equal s
 
 [<Fact>]
 let ``Test date time for keypath`` () =
   let dt = DateTime.Now
-  let jdoc = JsonDocument.Parse (sprintf """{"key": %s}""" (JsonSerializer.Serialize dt))
-  jdoc.RootElement
-  |> JPath.dateTime "key"
-  |> should equal dt
+  let jdoc = JsonDocument.Parse(sprintf """{"key": %s}""" (JsonSerializer.Serialize dt))
+  jdoc.RootElement |> JPath.dateTime "key" |> should equal dt
 
 [<Fact>]
 let ``Test date time offset for keypath`` () =
-  let dto = DateTimeOffset (DateTime.Now)
-  let jdoc = JsonDocument.Parse (sprintf """{"key": %s}""" (JsonSerializer.Serialize dto))
-  jdoc.RootElement
-  |> JPath.dateTimeOffset "key"
-  |> should equal dto
+  let dto = DateTimeOffset(DateTime.Now)
+  let jdoc = JsonDocument.Parse(sprintf """{"key": %s}""" (JsonSerializer.Serialize dto))
+  jdoc.RootElement |> JPath.dateTimeOffset "key" |> should equal dto
 
 [<Fact>]
 let ``Test guid for keypath`` () =
-  let guid = Guid.NewGuid ()
-  let jdoc = JsonDocument.Parse (sprintf """{"key": %s}""" (JsonSerializer.Serialize guid))
-  jdoc.RootElement
-  |> JPath.guid "key"
-  |> should equal guid
+  let guid = Guid.NewGuid()
+  let jdoc = JsonDocument.Parse(sprintf """{"key": %s}""" (JsonSerializer.Serialize guid))
+  jdoc.RootElement |> JPath.guid "key" |> should equal guid
